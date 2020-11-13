3a1c7a9d0e8b17453f3a6f55f7b023d4ce251340
Dmitry Peshkin <dmitrey04@gmail.com>
2020-11-13 18:50:12 +0500

    fix пропадания карты

--- src/sections/article/Article.Map.tsx
+++ src/sections/article/Article.Map.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { geoUriParse } from '../../helpers/geoUriParse';
@@ -8,23 +9,26 @@ interface Props {
 }
 
 export const ArticleMap: React.FC<Props> = ({ baloonTitle, geoUri }) => {
-  if (typeof window == 'undefined' || !geoUri || geoUriParse(geoUri) == null) {
+  if (!geoUri || geoUriParse(geoUri) == null) {
     return null;
   }
   const { latitude, longitude, zoom } = geoUriParse(geoUri)!;
 
   return (
-    <MapContainer
-      center={[latitude, longitude]}
-      zoom={zoom}
-      scrollWheelZoom={false}
-      style={{ height: '600px', margin: '40px auto 70px' }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[latitude, longitude]}>{baloonTitle && <Popup>{baloonTitle}</Popup>}</Marker>
-    </MapContainer>
+    <MapWrapper>
+      <MapContainer center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={false} style={{ height: '600px' }}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[latitude, longitude]}>{baloonTitle && <Popup>{baloonTitle}</Popup>}</Marker>
+      </MapContainer>
+    </MapWrapper>
   );
 };
+
+const MapWrapper = styled.div`
+  position: relative;
+  height: 600px;
+  margin: 40px auto 70px;
+`;
